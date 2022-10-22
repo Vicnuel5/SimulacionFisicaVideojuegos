57a7a81ec8f8bc15017f70a3f7d9c3ecd10f6494
Vicnuel5 <81177633+Vicnuel5@users.noreply.github.com>
2022-10-22 12:33:57 +0200

    a

--- skeleton/ParticleSystems/Geometric.cpp
+++ skeleton/ParticleSystems/Geometric.cpp
@@ -10,7 +10,10 @@ Geometric::Geometric(Vector3 std_dev_pos, float vel, int generations, int maxGen
 	ParticleGenerator(), std_dev_pos(std_dev_pos), vel(vel), cont(0),
 		generations(generations), maxGenerations(maxGenerations), shape(shape)
 {
-	particles = generateParticles();
+	if (generations == 0)
+		particles.push_back(new Particle(std_dev_pos, { 0, vel, 0 }, { 0, 0, 0 }, 0.95, 0.5));
+	else
+		particles = generateParticles();
 }
 
 void Geometric::p_Integrate(double t)
@@ -21,10 +24,8 @@ void Geometric::p_Integrate(double t)
 
 		if (generations < maxGenerations) {
 			for (auto i : particles) {
-				if (generations < maxGenerations) {
-					Geometric* f = new Geometric(i->getPos(), vel, generations + 1, maxGenerations, shape);
-					particle_generators.push_back(f);
-				}
+				Geometric* f = new Geometric(i->getPos(), vel, generations + 1, maxGenerations, shape);
+				particle_generators.push_back(f);
 			}
 		}
 
@@ -56,27 +57,20 @@ std::list<Particle*> Geometric::generateParticles() {
 
 	Vector4 coulour = { d(gen), d(gen), d(gen), 1 };
 
-	if (generations == 0) {
-		Particle* p = new Particle(std_dev_pos, {0, vel, 0}, {0, 0, 0}, 0.95, 0.5);
-		
+
+	double angle = 0.0f;
+	double incr = 360.0 / shape;
+
+	for (int i = 0; i < shape; i++) {
+
+		Particle* p = new Particle(std_dev_pos, { 0, 10, 0 }, { 0, 0, 0 }, 0.95, 0.5);
+		p->setVel({ (float) cos(angle * PI / 180.0) * vel, (float) sin(angle * PI / 180.0) * vel, 0 });
+		p->setColor(coulour);
 		l.push_back(p);
+
+		angle += incr;
 	}
-	else
-	{
-		double angle = 0.0f;
-		double incr = 360.0 / shape;
-
-		for (int i = 0; i < shape; i++) {
-
-			Particle* p = new Particle(std_dev_pos, { 0, 10, 0 }, { 0, 0, 0 }, 0.95, 0.5);
-			p->setVel({ (float) cos(angle * PI / 180.0) * vel, (float) sin(angle * PI / 180.0) * vel, 0 });
-			p->setColor(coulour);
-			l.push_back(p);
-
-			angle += incr;
-		}
-	}
-
+	
 	return l;
 }
 
